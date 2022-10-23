9cc9504f245563c7d3609df31cabdd6170a1dd41
hthuong1903 <thuongnth1903@gmail.com>
2022-10-23 16:50:12 +0700

    feat: provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import { Link, Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import NotFoundPage from './components/Common/NotFoundPage'
 import AdminLayout from './components/Layouts/AdminLayout'
 import UserLayout from './components/Layouts/UserLayout'
+import Category from './pages/Admin/Category'
+import Contract from './pages/Admin/Contract'
 import Product from './pages/Admin/Product'
+import Provider from './pages/Admin/Provider'
 import Receipt from './pages/Admin/Receipt'
 import TimeKeeping from './pages/Admin/TimeKeeping'
 import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const location = useLocation()
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<Product />} />
-          <Route path="receipts" element={<Receipt />} />
-          <Route path="timekeeping" element={<TimeKeeping />} />
-        </Route>
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Login />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<Category />} />
+                    <Route path="products" element={<Product />} />
+                    <Route path="suppliers" element={<Provider />} />
+                    <Route path="receipts" element={<Receipt />} />
+                    <Route path="timekeeping" element={<TimeKeeping />} />
+                    <Route path="contracts" element={<Contract />} />
+                </Route>
+                <Route path="/" element={<UserLayout />}>
+                    <Route index element={<Login />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
