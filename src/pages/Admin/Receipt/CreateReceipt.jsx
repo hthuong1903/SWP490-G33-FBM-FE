8e6116598da5595c21649ed9cc825ab63ef22c6d
TDQuang95 <tranducquang09052000@gmail.com>
2022-11-01 00:09:05 +0700

    Ghép API invoice

--- src/pages/Admin/Receipt/CreateReceipt.jsx
+++ src/pages/Admin/Receipt/CreateReceipt.jsx
@@ -1,4 +1,5 @@
 import authApi from '@/api/authApi'
+import orderApi from '@/api/orderApi'
 import productApi from '@/api/productApi'
 import StyledTableCell from '@/components/Common/Table/StyledTableCell'
 import StyledTableRow from '@/components/Common/Table/StyledTableRow'
@@ -9,9 +10,14 @@ import {
     Autocomplete,
     Button,
     Collapse,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
     Grid,
     IconButton,
     InputAdornment,
+    Radio,
+    RadioGroup,
     TextField,
     Typography
 } from '@mui/material'
@@ -23,10 +29,10 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import { Box } from '@mui/system'
 import { useEffect, useMemo, useState } from 'react'
-import { Controller } from 'react-hook-form'
-import NumberFormat from 'react-number-format'
-import { useLocation, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import './style.css'
+import moment from 'moment/moment'
 
 function CreateReceipt() {
     const [checkedProduct, setCheckedProduct] = useState(false)
@@ -36,8 +42,18 @@ function CreateReceipt() {
     const [selectedEmployee, setSelectedEmployee] = useState(null)
     const [selectedProduct, setSelectedProduct] = useState([])
     const [rows, setRows] = useState([])
+    const [value, setValue] = useState(1)
+    let navigate = useNavigate()
 
-    const location = useLocation()
+    const handleSelect = (event) => {
+        setValue(event.target.value)
+    }
+
+    const addressDetails = (selectedEmployee) =>
+        selectedEmployee.address
+            ? `${selectedEmployee.address}, ${selectedEmployee.districtName}, ${selectedEmployee.wardName}, ${selectedEmployee.provinceName}`
+            : 'Khách hàng chưa có địa chỉ'
+
     const handleChange = (event, value) => setSelectedEmployee(value)
 
     const min = 1
@@ -56,6 +72,7 @@ function CreateReceipt() {
         const getAllUser = async () => {
             try {
                 const response = await authApi.getAllUser()
+                console.log(response.data)
                 setUserList(response.data)
             } catch (error) {
                 console.log('fail at getAllUser', error)
@@ -64,7 +81,7 @@ function CreateReceipt() {
 
         getAllUser()
         getAllProduct()
-        setRows([...location.state[0].orderProductDtos])
+        // setRows([...location.state[0].orderProductDtos])
     }, [location])
 
     const formatProductList = productList.map((i) => {
@@ -80,14 +97,17 @@ function CreateReceipt() {
             changedPrice: i.discount
         }
     })
+
     useEffect(() => {
         console.log('rows', rows)
     }, [rows])
+
     const handleSelectedProduct = (event, value) => {
         console.log(value)
-        setSelectedProduct(value)
+        const quantity = { ...value, quantity: 1 }
+        setSelectedProduct(quantity)
         if (value) {
-            setRows([value, ...rows])
+            setRows([quantity, ...rows])
             setProductList((prev) => prev.filter((item) => item.id !== value.productId))
         }
     }
@@ -96,33 +116,72 @@ function CreateReceipt() {
         () => rows.reduce((result, value) => result + value.changedPrice, 0),
         [rows]
     )
-    // const resultDiscount = () => rows.reduce((result, value) => result + value.discount, 0)
 
+    const totalPriceOut = useMemo(
+        () => rows.reduce((result, value) => result + value.product.priceOut, 0),
+        [rows]
+    )
+    const totalQuantity = useMemo(
+        () => rows.reduce((result, value) => result + value.quantity, 0),
+        [rows]
+    )
+
+    const totalAmount = rows.reduce(
+        (result, value) => result + value.quantity * value.product.priceOut,
+        0
+    )
+    const totalAmountAfter = rows.reduce(
+        (result, value) => result + value.quantity * value.product.priceOut - value.changedPrice,
+        0
+    )
+    // const resultDiscount = () => rows.reduce((result, value) => result + value.discount, 0)
+    const handleCreate = async () => {
+        const dataSubmit = {
+            customerId: selectedEmployee.id,
+            dateCreated: moment(new Date()).format('YYYY-MM-DD'),
+            dateRequired: moment(new Date()).format('YYYY-MM-DD'),
+            employeeSaleId: 2,
+            id: null,
+            numberOfProducts: rows.length,
+            orderProductDtos: rows,
+            status: 3,
+            totalOrderPrice: totalPriceOut * totalQuantity,
+            totalOrderPriceAfter: totalPriceOut * totalQuantity - totalDiscount,
+            typeOfPay: value
+        }
+        try {
+            const response = await orderApi.createOrderT(dataSubmit)
+            if (response.data.data) {
+                orderApi
+                    .createQuoteT(selectedEmployee.id, dataSubmit)
+                    .then((res) => console.log(res))
+                    .catch((error) => console.log(error))
+                toast.success(response.data.message)
+                navigate(-1)
+            } else {
+                toast.error(response.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        console.log(dataSubmit)
+    }
     return (
         <>
-            <h2>Chi tiết đơn hàng</h2>
+            <h2>Tạo hóa đơn</h2>
             <Box
                 sx={{
                     my: 3,
                     display: 'flex',
                     justifyContent: 'space-between',
                     alignItems: 'center'
-                }}>
-                <Typography sx={{ fontWeight: 'bold' }}>
-                    Thông tin chi tiết đơn hàng {orderId}
-                </Typography>
-                <Box sx={{ display: 'flex', gap: '12px' }}>
-                    <Button variant="contained">Xem báo giá</Button>
-                    <Button variant="contained">Tạo báo giá</Button>
-                    <Button variant="contained">Tạo hóa đơn</Button>
-                </Box>
-            </Box>
+                }}></Box>
             <Collapse in={Boolean(rows)}>
                 <TableContainer component={Paper} sx={{ maxHeight: '380px' }}>
                     <Table sx={{ minWidth: 700 }} stickyHeader aria-label="sticky table">
                         <TableHead>
                             <TableRow>
-                                <StyledTableCell align="left">Sản phẩm</StyledTableCell>
+                                <StyledTableCell align="left">Tên sản phẩm</StyledTableCell>
                                 <StyledTableCell align="left">Giá bán</StyledTableCell>
                                 <StyledTableCell align="center">Số lượng</StyledTableCell>
                                 <StyledTableCell align="left">Chiết khấu</StyledTableCell>
@@ -167,22 +226,16 @@ function CreateReceipt() {
                                             {row?.product.priceOut.toLocaleString('vi-vn')} VND
                                         </StyledTableCell>
                                         <StyledTableCell align="center">
-                                            <Box>
+                                            <Box sx={{ display: 'flex' }}>
                                                 <IconButton
                                                     onClick={() => {
                                                         let newQuantity = Number(row.quantity) - 1
-                                                        console.log(row.productId)
                                                         if (newQuantity < 0) return
                                                         setRows((oldRow) => {
                                                             return oldRow.map((item) => {
                                                                 if (
                                                                     item.productId === row.productId
                                                                 ) {
-                                                                    console.log('123', {
-                                                                        ...item,
-                                                                        quantity: newQuantity
-                                                                    })
-
                                                                     return {
                                                                         ...item,
                                                                         quantity: newQuantity
@@ -253,15 +306,6 @@ function CreateReceipt() {
                                                 sx={{ width: '250px' }}
                                                 onChange={(event) => {
                                                     row.changedPrice = event.target.value
-                                                }}
-                                                InputProps={{
-                                                    endAdornment: (
-                                                        <InputAdornment position="end">
-                                                            VND
-                                                        </InputAdornment>
-                                                    )
-                                                }}
-                                                onBlur={() => {
                                                     let newQuantity = Number(row.changedPrice)
                                                     setRows((oldRow) => {
                                                         return oldRow.map((item) => {
@@ -275,6 +319,27 @@ function CreateReceipt() {
                                                         })
                                                     })
                                                 }}
+                                                InputProps={{
+                                                    endAdornment: (
+                                                        <InputAdornment position="end">
+                                                            VND
+                                                        </InputAdornment>
+                                                    )
+                                                }}
+                                                // onBlur={() => {
+                                                //     let newQuantity = Number(row.changedPrice)
+                                                //     setRows((oldRow) => {
+                                                //         return oldRow.map((item) => {
+                                                //             if (item.id === row.id) {
+                                                //                 return {
+                                                //                     ...item,
+                                                //                     changedPrice: newQuantity
+                                                //                 }
+                                                //             }
+                                                //             return item
+                                                //         })
+                                                //     })
+                                                // }}
                                             />
                                         </StyledTableCell>
                                         <StyledTableCell align="left">
@@ -307,7 +372,6 @@ function CreateReceipt() {
                                                         },
                                                         ...productList
                                                     ])
-                                                    console.log(row)
                                                 }}>
                                                 <DeleteIcon />
                                             </IconButton>
@@ -381,7 +445,13 @@ function CreateReceipt() {
                                     <Autocomplete
                                         id="tags-outlined"
                                         options={userList}
-                                        getOptionLabel={(option) => option.name}
+                                        getOptionLabel={(option) =>
+                                            option.firstName +
+                                            ' ' +
+                                            option.middleName +
+                                            ' ' +
+                                            option.lastName
+                                        }
                                         onChange={handleChange}
                                         filterSelectedOptions
                                         sx={{ width: 300, mt: 1, ml: 4 }}
@@ -390,7 +460,8 @@ function CreateReceipt() {
                                                 component="li"
                                                 sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
                                                 {...props}>
-                                                {option.name} - {option.phone}
+                                                {option.firstName} {option.middleName}{' '}
+                                                {option.lastName}- {option.phone}
                                                 <br />
                                                 {option.roles[0].name}
                                             </Box>
@@ -406,27 +477,85 @@ function CreateReceipt() {
                                     />
                                 )}
                             </Collapse>
+                            {selectedEmployee && (
+                                <Box sx={{ mt: 2, mr: 3 }}>
+                                    {/* <Typography variant="subtitle1">
+                                    <b>THÔNG TIN KHÁCH HÀNG</b>
+                                </Typography> */}
+                                    <hr />
+                                    <Typography variant="subtitle1">
+                                        {selectedEmployee.firstName +
+                                            ' ' +
+                                            selectedEmployee.middleName +
+                                            ' ' +
+                                            selectedEmployee.lastName}
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        <b>Địa chỉ: </b>
+                                        {addressDetails(selectedEmployee)}
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        <b>SĐT: </b> {selectedEmployee.phone}
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        <b>Email: </b> {selectedEmployee.email}
+                                    </Typography>
+                                </Box>
+                            )}
                         </Box>
                     </Grid>
                     <Grid item xs={4}>
                         <table>
-                            <tr>
-                                <td>Tổng</td>
-                                <td>{rows.length}</td>
-                            </tr>
-                            <tr>
-                                <td>Tổng tiền chiết khấu</td>
-                                <td>{totalDiscount.toLocaleString('vi-VN')} VND</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <b>Tổng tiền</b>
-                                </td>
-                                <td>
-                                    <b>5</b>
-                                </td>
-                            </tr>
+                            <tbody>
+                                <tr>
+                                    <td>Tổng</td>
+                                    <td>{totalAmount.toLocaleString('vi-VN')} VND</td>
+                                </tr>
+                                <tr>
+                                    <td>Tổng tiền chiết khấu</td>
+                                    <td>{totalDiscount.toLocaleString('vi-VN')} VND</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <b>Tổng tiền</b>
+                                    </td>
+                                    <td>
+                                        <b>{totalAmountAfter.toLocaleString('vi-VN')} VND</b>
+                                    </td>
+                                </tr>
+                            </tbody>
                         </table>
+                        <hr />
+                        <FormControl sx={{ width: '100%' }}>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="1"
+                                name="timesheet"
+                                value={value}
+                                onChange={handleSelect}>
+                                <FormLabel
+                                    id="demo-radio-buttons-group-label"
+                                    sx={{ display: 'block' }}>
+                                    Hình thức thanh toán
+                                </FormLabel>
+                                <FormControlLabel value="1" control={<Radio />} label="Tiền mặt" />
+                                <FormControlLabel
+                                    value="2"
+                                    control={<Radio />}
+                                    label="Chuyển khoản"
+                                />
+                                <FormControlLabel value="3" control={<Radio />} label="Quẹt thẻ" />
+                                <FormControlLabel value="4" control={<Radio />} label="COD" />
+                            </RadioGroup>
+                        </FormControl>
+                        <Box sx={{ display: 'flex', gap: '12px' }}>
+                            <Button variant="contained" onClick={() => navigate(-1)}>
+                                Hủy
+                            </Button>
+                            <Button variant="contained" onClick={() => handleCreate(3)}>
+                                Tạo hóa đơn
+                            </Button>
+                        </Box>
                     </Grid>
                 </Grid>
             </Paper>
