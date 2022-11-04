9c1af829cd4f93badc3f90223be5b423d00dcf61
hthuong1903 <thuongnth1903@gmail.com>
2022-11-04 20:59:28 +0700

    feat: loading screen

--- src/pages/Admin/Order/OrderDetails/index.jsx
+++ src/pages/Admin/Order/OrderDetails/index.jsx
@@ -14,17 +14,22 @@ import { useEffect, useState } from 'react'
 import orderApi from '@/api/orderApi'
 import { ORDER_STATUS, PAYMENT_METHOD } from '../constant'
 import moment from 'moment'
+import Loading from '@/components/Common/Loading'
 
 export default function OrderDetails() {
     const [orderDetail, setOrderDetail] = useState([])
     let { orderId } = useParams()
     const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const getOrderById = async (orderId) => {
             try {
                 const response = await orderApi.getAllOrderById(orderId)
-                setOrderDetail(response.data)
+                if (response.data) {
+                    setIsLoading(false)
+                    setOrderDetail(response.data)
+                }
             } catch (error) {
                 console.log('fail when getOrderById', error)
             }
@@ -56,163 +61,172 @@ export default function OrderDetails() {
             0
         )
 
-    return (
-        <>
-            <h2>Chi tiết đơn hàng</h2>
-            <Box
-                sx={{
-                    my: 3,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                }}>
-                <Typography sx={{ fontWeight: 'bold' }}>
-                    Thông tin chi tiết đơn hàng {orderId}
-                </Typography>
-                <Box sx={{ display: 'flex', gap: '12px' }}>
-                    <Button variant="contained">Xem báo giá</Button>
-                    {orderDetail[0]?.status !== 3 && orderDetail[0]?.status !== 4 ? (
-                        <Button
-                            variant="contained"
-                            onClick={() =>
-                                navigate('../orders/createOrder', { state: orderDetail })
-                            }>
-                            Tạo báo giá
-                        </Button>
-                    ) : null}
-                </Box>
-            </Box>
-            <TableContainer component={Paper} sx={{ maxHeight: '380px' }}>
-                <Table sx={{ minWidth: 700 }} stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="left">Sản phẩm</StyledTableCell>
-                            <StyledTableCell align="left">Giá bán</StyledTableCell>
-                            <StyledTableCell align="left">Số lượng</StyledTableCell>
-                            <StyledTableCell align="left">Chiết khấu</StyledTableCell>
-                            <StyledTableCell align="left">Thành tiền</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {orderDetail[0] &&
-                            orderDetail[0].orderProductDtos.map((row) => (
-                                <StyledTableRow key={row.productId}>
-                                    <StyledTableCell align="left">
-                                        <Box
-                                            sx={{
-                                                display: 'flex',
-                                                gap: '15px',
-                                                alignItems: 'center'
-                                            }}>
-                                            <img
-                                                src={`http://api.dinhtruong.live/api/storage_server/download/${row?.product.photoMain}`}
-                                                alt="123"
-                                                width="100px"
-                                            />
-                                            <Box>
-                                                <Typography sx={{ fontWeight: 'bold' }}>
-                                                    {row?.product.name}
-                                                </Typography>
-                                                <Typography variant="button">
-                                                    SKU: {row?.product.productCode}
-                                                </Typography>
-                                            </Box>
-                                        </Box>
-                                    </StyledTableCell>
-                                    <StyledTableCell align="left">
-                                        {row?.product.priceOut.toLocaleString('vi-vn')} VND
-                                    </StyledTableCell>
-                                    <StyledTableCell align="left">{row?.quantity}</StyledTableCell>
-                                    <StyledTableCell align="left">
-                                        {row?.changedPrice.toLocaleString('vi-vn')} VND
-                                    </StyledTableCell>
-                                    <StyledTableCell align="left">
-                                        <b>
-                                            {(
-                                                row?.product.priceOut * row?.quantity -
-                                                row?.changedPrice
-                                            ).toLocaleString('vi-VN')}{' '}
-                                            VND
-                                        </b>
-                                    </StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <Paper>
+    useEffect(() => {
+        console.log(isLoading)
+    }, [isLoading])
+
+    if (isLoading) return <Loading />
+    else
+        return (
+            <>
+                <h2>Chi tiết đơn hàng</h2>
                 <Box
                     sx={{
+                        my: 3,
                         display: 'flex',
-                        justifyContent: 'flex-end',
-                        my: 2,
-                        pt: 1
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
                     }}>
-                    <table>
-                        <tr>
-                            <td className="td">Tổng</td>
-                            <td>{totalAmount?.toLocaleString('vi-VN')} VND</td>
-                        </tr>
-                        <tr>
-                            <td>Tổng tiền chiết khấu</td>
-                            <td>
-                                {orderDetail[0]?.totalOrderPriceAfter.toLocaleString('vi-VN')} VND
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <b>Tổng tiền</b>
-                            </td>
-                            <td>
-                                <b>{totalAmountAfter?.toLocaleString('vi-VN')} VND</b>
-                            </td>
-                        </tr>
-                    </table>
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                        Thông tin chi tiết đơn hàng {orderId}
+                    </Typography>
+                    <Box sx={{ display: 'flex', gap: '12px' }}>
+                        <Button variant="contained">Xem báo giá</Button>
+                        {orderDetail[0]?.status !== 3 && orderDetail[0]?.status !== 4 ? (
+                            <Button
+                                variant="contained"
+                                onClick={() =>
+                                    navigate('../orders/createOrder', { state: orderDetail })
+                                }>
+                                Tạo báo giá
+                            </Button>
+                        ) : null}
+                    </Box>
                 </Box>
-                <Divider />
-                <Grid container sx={{ px: 4, py: 2 }}>
-                    <Grid item xs={4}>
-                        <Box>
+                <TableContainer component={Paper} sx={{ maxHeight: '380px' }}>
+                    <Table sx={{ minWidth: 700 }} stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell align="left">Sản phẩm</StyledTableCell>
+                                <StyledTableCell align="left">Giá bán</StyledTableCell>
+                                <StyledTableCell align="left">Số lượng</StyledTableCell>
+                                <StyledTableCell align="left">Chiết khấu</StyledTableCell>
+                                <StyledTableCell align="left">Thành tiền</StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {orderDetail[0] &&
+                                orderDetail[0].orderProductDtos.map((row) => (
+                                    <StyledTableRow key={row.productId}>
+                                        <StyledTableCell align="left">
+                                            <Box
+                                                sx={{
+                                                    display: 'flex',
+                                                    gap: '15px',
+                                                    alignItems: 'center'
+                                                }}>
+                                                <img
+                                                    src={`http://api.dinhtruong.live/api/storage_server/download/${row?.product.photoMain}`}
+                                                    alt="123"
+                                                    width="100px"
+                                                />
+                                                <Box>
+                                                    <Typography sx={{ fontWeight: 'bold' }}>
+                                                        {row?.product.name}
+                                                    </Typography>
+                                                    <Typography variant="button">
+                                                        SKU: {row?.product.productCode}
+                                                    </Typography>
+                                                </Box>
+                                            </Box>
+                                        </StyledTableCell>
+                                        <StyledTableCell align="left">
+                                            {row?.product.priceOut.toLocaleString('vi-vn')} VND
+                                        </StyledTableCell>
+                                        <StyledTableCell align="left">
+                                            {row?.quantity}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="left">
+                                            {row?.changedPrice.toLocaleString('vi-vn')} VND
+                                        </StyledTableCell>
+                                        <StyledTableCell align="left">
+                                            <b>
+                                                {(
+                                                    row?.product.priceOut * row?.quantity -
+                                                    row?.changedPrice
+                                                ).toLocaleString('vi-VN')}{' '}
+                                                VND
+                                            </b>
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <Paper>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            my: 2,
+                            pt: 1
+                        }}>
+                        <table>
+                            <tr>
+                                <td className="td">Tổng</td>
+                                <td>{totalAmount?.toLocaleString('vi-VN')} VND</td>
+                            </tr>
+                            <tr>
+                                <td>Tổng tiền chiết khấu</td>
+                                <td>
+                                    {orderDetail[0]?.totalOrderPriceAfter.toLocaleString('vi-VN')}{' '}
+                                    VND
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Tổng tiền</b>
+                                </td>
+                                <td>
+                                    <b>{totalAmountAfter?.toLocaleString('vi-VN')} VND</b>
+                                </td>
+                            </tr>
+                        </table>
+                    </Box>
+                    <Divider />
+                    <Grid container sx={{ px: 4, py: 2 }}>
+                        <Grid item xs={4}>
+                            <Box>
+                                <Typography variant="subtitle1">
+                                    <b>THÔNG TIN KHÁCH HÀNG</b>
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    {orderDetail[0]?.customer.username}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    <b>Địa chỉ: </b> {addressDetails()}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    <b>SĐT: </b> {orderDetail[0]?.customer.phone}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    <b>Email: </b> {orderDetail[0]?.customer.email}
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={4}>
                             <Typography variant="subtitle1">
-                                <b>THÔNG TIN KHÁCH HÀNG</b>
+                                <b>Hình thức thanh toán: </b> {paymentMethod()}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Typography variant="subtitle1">
+                                <b>Ngày tạo đơn hàng: </b>
+                                {moment(orderDetail[0]?.dateCreated).format('DD/MM/YYYY')}
                             </Typography>
                             <Typography variant="subtitle1">
-                                {orderDetail[0]?.customer.username}
+                                <b>Tình trạng đơn hàng:</b>{' '}
+                                <Chip
+                                    label={orderStatus()?.name}
+                                    sx={{
+                                        border: `1px solid ${orderStatus()?.color}`,
+                                        color: `${orderStatus()?.color}`
+                                    }}
+                                />
                             </Typography>
-                            <Typography variant="subtitle1">
-                                <b>Địa chỉ: </b> {addressDetails()}
-                            </Typography>
-                            <Typography variant="subtitle1">
-                                <b>SĐT: </b> {orderDetail[0]?.customer.phone}
-                            </Typography>
-                            <Typography variant="subtitle1">
-                                <b>Email: </b> {orderDetail[0]?.customer.email}
-                            </Typography>
-                        </Box>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Typography variant="subtitle1">
-                            <b>Hình thức thanh toán: </b> {paymentMethod()}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Typography variant="subtitle1">
-                            <b>Ngày tạo đơn hàng: </b>
-                            {moment(orderDetail[0]?.dateCreated).format('DD/MM/YYYY')}
-                        </Typography>
-                        <Typography variant="subtitle1">
-                            <b>Tình trạng đơn hàng:</b>{' '}
-                            <Chip
-                                label={orderStatus()?.name}
-                                sx={{
-                                    border: `1px solid ${orderStatus()?.color}`,
-                                    color: `${orderStatus()?.color}`
-                                }}
-                            />
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Paper>
-        </>
-    )
+                </Paper>
+            </>
+        )
 }
