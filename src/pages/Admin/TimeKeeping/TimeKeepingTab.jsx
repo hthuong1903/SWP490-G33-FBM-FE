d68603e1cec0691b83748b39031e8e314a13cb32
hthuong1903 <thuongnth1903@gmail.com>
2022-12-04 01:42:11 +0700

    fix: time keeping

--- src/pages/Admin/TimeKeeping/TimeKeepingTab.jsx
+++ src/pages/Admin/TimeKeeping/TimeKeepingTab.jsx
@@ -81,8 +81,7 @@ function TimeKeepingTab({ value, index, periodCode }) {
         { field: 'holidaysWorking', headerName: 'CÔNG LỄ', flex: 0.75 },
         { field: 'haftDayWorking', headerName: 'CÔNG NỬA NGÀY', flex: 1 },
         { field: 'totalDayWorking', headerName: 'TỔNG SỐ NGÀY LÀM VIỆC', flex: 1 },
-        // { field: 'workingDay', headerName: 'NGÀY THƯỜNG', flex: 1 },
-        { field: 'totalDayWorking', headerName: 'TỔNG SỐ NGÀY LÀM VIỆC', flex: 1.25 },
+        { field: 'workingDay', headerName: 'NGÀY THƯỜNG', flex: 1 },
         {
             field: 'actions',
             headerName: 'TÁC VỤ',
