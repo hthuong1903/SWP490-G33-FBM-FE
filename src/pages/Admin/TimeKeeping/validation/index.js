2c5921243dee1332b643abe56ffb2b70d44ebb3f
hthuong1903 <thuongnth1903@gmail.com>
2022-12-09 12:58:27 +0700

    fix: bug-20

--- src/pages/Admin/TimeKeeping/validation/index.js
+++ src/pages/Admin/TimeKeeping/validation/index.js
@@ -2,8 +2,13 @@ import yup from "@/utils/yupValidation"
 
 export const schemaOvertime = yup.object().shape({
   hour: yup.number().required('Vui lòng điền vào trường này.').typeError('Vui lòng nhập số'),
-  content: yup.string().required('Vui lòng điền vào trường này.')
-
+  content: yup.string().required('Vui lòng điền vào trường này.'),
+  hour: yup
+    .number()
+    .required('Vui lòng điền vào trường này.')
+    .min(0, 'Vui lòng nhập giá trị lớn hơn 0')
+    .typeError('Vui lòng nhập giá trị lớn hơn 0')
+    .max(8, 'Giá trị không hợp lệ'),
 })
 export const schemaTimeKeeping = yup.object().shape({
   date: yup.date().nullable().required('Vui lòng điền vào trường này.').typeError('Vui lòng nhập định dạng dd/MM/yyyy'),
