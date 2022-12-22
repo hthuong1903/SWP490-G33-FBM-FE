f26025133d39d90b27590d58dc412fd32b9f6879
hthuong1903 <thuongnth1903@gmail.com>
2022-12-23 01:34:37 +0700

    fix: date picker overtime

--- src/pages/Admin/TimeKeeping/components/ModalOvertime.jsx
+++ src/pages/Admin/TimeKeeping/components/ModalOvertime.jsx
@@ -10,9 +10,31 @@ import { schemaOvertime } from '../validation'
 import { toast } from 'react-toastify'
 import axios from 'axios'
 import Constants from '@/components/Constants'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { vi } from 'date-fns/locale'
+// import { toast } from 'react-toastify'
+import moment from 'moment'
+import { useState } from 'react'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { Controller } from 'react-hook-form'
+
+function disablePrevDates(startDate) {
+    const startSeconds = Date.parse(startDate)
+    return (date) => {
+        return Date.parse(date) < startSeconds
+    }
+}
+
+function getFirstDayOfMonth(year, month) {
+    return new Date(year, month, 1)
+}
 
 function ModalOvertime({ title, content, isOpen, handleClose, handleConfirm, employee }) {
+    const [value, setValue] = useState(1)
+    var today = new Date()
+    const firstDayCurrentMonth = getFirstDayOfMonth(today.getFullYear(), today.getMonth())
     const {
+        control,
         register,
         handleSubmit,
         formState: { errors }
@@ -21,11 +43,16 @@ function ModalOvertime({ title, content, isOpen, handleClose, handleConfirm, emp
         resolver: yupResolver(schemaOvertime)
     })
 
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
     const updateOvertime = async (data) => {
         try {
             await axios.put(Constants.baseAPI + 'api/overtime', data).then((res) => {
                 console.log(res)
                 toast.success(res.message)
+                handleClose && handleClose()
             })
             handleClose && handleClose()
         } catch (error) {
@@ -35,7 +62,7 @@ function ModalOvertime({ title, content, isOpen, handleClose, handleConfirm, emp
 
     const onSubmit = (data) => {
         //+ employee.hour
-        const dataSubmit = { ...employee, hour: data.hour + employee.hour, content: data.content }
+        const dataSubmit = { ...employee, hour: data.hour, content: data.content, date: moment(data.date).format('yyyy-MM-DD')}
         updateOvertime(dataSubmit)
     }
     return (
@@ -49,6 +76,57 @@ function ModalOvertime({ title, content, isOpen, handleClose, handleConfirm, emp
             <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
 
             <DialogContent>
+                <Grid container sx={{ mt: 1 }} spacing={4}>
+                    <Grid item xs={4} sx={{ '& .MuiTextField-root': { mb: 2 } }}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={vi}>
+                            <Box>
+                                <Controller
+                                    required
+                                    name="date"
+                                    control={control}
+                                    defaultValue={new Date()}
+                                    render={({
+                                        field: { onChange, value },
+                                        fieldState: { error }
+                                    }) => (
+                                        <DatePicker
+                                            label="Chọn ngày công"
+                                            disableFuture
+                                            shouldDisableDate={disablePrevDates(
+                                                firstDayCurrentMonth
+                                            )}
+                                            ampm={false}
+                                            value={value}
+                                            // onChange={(value) => onChange(value)}
+                                            // value={new Date(datePlayerDeadline)}
+                                            onChange={(value) => {
+                                                onChange(value)
+                                                // setDatePlayerDeadline(value)
+                                            }}
+                                            renderInput={(params) => (
+                                                <TextField
+                                                    sx={{
+                                                        marginTop: '0px !important',
+                                                        marginBottom: '16px !important'
+                                                    }}
+                                                    {...params}
+                                                    required
+                                                    id="outlined-disabled"
+                                                    error={!!error}
+                                                    helperText={error ? error.message : null}
+                                                    // id="startDate"
+                                                    variant="outlined"
+                                                    margin="dense"
+                                                    fullWidth
+                                                />
+                                            )}
+                                        />
+                                    )}
+                                />
+                            </Box>
+                        </LocalizationProvider>
+                    </Grid>
+                </Grid>
                 <Grid container sx={{ mt: 1 }} spacing={2}>
                     <Grid item xs={6} sx={{ '& .MuiTextField-root': { mb: 2 } }}>
                         <Box>
